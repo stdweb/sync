a4cc25e4f001afdc4050d7ea5a3ec99c463463c4
std <std@std.lv>
2015-11-04 18:43:51 +0200

    check perf acc ledg

--- src/main/java/stdweb/Core/LedgerStore.java
+++ src/main/java/stdweb/Core/LedgerStore.java
@@ -410,6 +410,7 @@ public class LedgerStore {
         statement.execute("create table if not exists ledger(id identity primary key, tx binary(32),address binary(20),amount decimal(31,0),block bigint,blocktimestamp timestamp," +
                 "depth tinyint,gasused bigint,fee decimal(31,0),entryType tinyint,entryResult tinyint,offsetaccount binary(20),descr varchar(32),GrossAmount decimal(31,0))");
         statement.execute("create index if not exists idx_ledger_address_tx on ledger(address,tx)");
+        statement.execute("create index if not exists idx_ledger_address_id on ledger(address,id)");
         statement.execute("create index if not exists idx_ledger_tx on ledger(tx)");
         statement.execute("create index if not exists idx_ledger_block_id on ledger(block,id)");
 
